ef946c372df031e16d471d1beeecff0a4af91cde
Ryan Modrak <rmodrak@gmail.com>
2017-11-22 03:40:15 -0900

    Initial commit

---
+++ asdf_converters/sac2asdf.py
@@ -0,0 +1,156 @@
+
+import argparse
+import collections
+import io
+import glob
+
+import numpy as np
+import obspy
+
+from os.path import exists, join
+from asdf_converters.util.sac import read_sac_files, get_event_coords, get_station_coords
+from pyasdf import ASDFDataSet
+
+
+
+def getargs():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('input', type=str,
+        help='SAC input directory')
+
+    parser.add_argument('output', type=str,
+        help='ASDF output filename')
+
+    parser.add_argument('tag', type=str,
+        help='ASDF tag')
+
+    return parser.parse_args()
+
+
+def get_event_id():
+    return obspy.core.event.ResourceIdentifier()
+
+
+def sort_by_station(channels):
+    stations = collections.defaultdict(list)
+    for trace_id, coords in channels.items():
+        network, station, location_code, channel = trace_id.split('.')
+        stations[(network, station)] += [(location_code, channel,) + coords]
+    return stations.items()
+
+
+def get_name(trace):
+    return '.'.join([
+        trace.stats.network,
+        trace.stats.station,
+        trace.stats.channel])
+
+
+def write_bytesio(data):
+    f = io.BytesIO()
+    _Pickler(f, protocol=None, fix_imports=fix_imports).dump(data)
+    return f.getvalue()
+
+    
+
+
+# SAC2ASDF
+if __name__=='__main__':
+    args = getargs()
+
+    if not exists(args.input):
+        raise Exception("File not found:." % args.input)
+
+    if exists(args.output):
+        raise Exception("File exists: %s" % args.output)
+
+    wildcard = '*.sac'
+    files = glob.glob(join(args.input, wildcard))
+    stream = read_sac_files(files)
+    ds = ASDFDataSet(args.output, mode='a')
+
+    headers = {}
+    events = {}
+    channels = {}
+
+    starttime = np.inf
+    endtime = -np.inf
+
+    for trace in stream:
+        # keep track of headers
+        sac_header = trace.stats.sac
+        sac_filename = trace.sac_filename
+        headers[sac_filename] = sac_header
+
+        # keep track of events
+        coords = get_event_coords(sac_header)
+        if coords in events:
+            event_id = events[coords]
+        else:
+            event_id = get_event_id()
+            events[coords] = event_id
+
+        # keep track of channels
+        coords = get_station_coords(sac_header)
+        name = trace.id
+        channels[name] = coords
+
+        # keep track of recording times
+        s, e = trace.stats.starttime, trace.stats.endtime
+        if s.timestamp < starttime:
+            starttime = s
+        if e.timestamp > endtime:
+            endtime = e
+
+        # add waveform
+        ds.add_waveforms(trace, args.tag, event_id)
+
+    # add events
+    catalog = obspy.core.event.Catalog()
+    for event_coords, event_id in events.items():
+        latitude, longitude, depth, origin_time = event_coords
+        origin = obspy.core.event.Origin(
+            time=origin_time,
+            longitude=longitude,
+            latitude=latitude,
+            depth=depth)
+        catalog.append(obspy.core.event.Event(
+            resource_id=event_id,
+            origins=[origin]))
+    ds.add_quakeml(catalog)
+
+    # add stations
+    for group1, group2 in sort_by_station(channels):
+        network, station = group1
+        for location_code, channel, latitude, longitude, depth, elevation in group2:
+            inventory = obspy.core.inventory.Inventory(
+                networks=[obspy.core.inventory.Network(
+                    code=network, 
+                    stations=[obspy.core.inventory.Station(
+                        code=station, 
+                        latitude=group2[0][2],
+                        longitude=group2[0][3],
+                        elevation=group2[0][4],
+                        start_date=starttime,
+                        end_date=endtime,
+                        creation_date=obspy.UTCDateTime(),
+                        site=obspy.core.inventory.Site(name=""),
+                        channels=[obspy.core.inventory.Channel(
+                            code=channel,
+                            location_code=location_code,
+                            latitude=latitude,
+                            longitude=longitude,
+                            elevation=elevation,
+                            depth=depth,
+                            start_date=starttime,
+                            end_date=endtime)])])],                 
+                source="pyasdf sac2asdf converter")
+            ds.add_stationxml(inventory)
+
+    # add sac_headers as auxiliary data
+    ds.add_auxiliary_data_file(write_bytesio(headers),
+       path='dummy_path')
+
+    del ds
+
---
+++ asdf_converters/util/sac.py
@@ -0,0 +1,127 @@
+
+import glob
+import os
+import warnings
+
+from os.path import join
+
+import numpy as np
+import obspy
+from obspy.core import Stream, Stats, Trace
+from obspy.core.event import Event, Origin
+from obspy.core.inventory import Inventory, Station
+
+
+def read_sac_files(files):
+    stream = obspy.core.Stream()
+    for filename in files:
+        try:
+            _fromfile = obspy.read(filename, format='sac')
+            trace = _fromfile[0]
+        except:
+            print("\nFailed to read '%s' as a SAC file." % filename)
+            continue
+        trace.sac_filename = filename
+        stream += _fromfile
+
+    return stream
+
+
+def get_event_coords(sac_headers):
+    # location
+    try:
+        latitude = sac_headers.evla
+        longitude = sac_headers.evlo
+    except (TypeError, ValueError):
+        warnings.warn("Could not determine event location from sac headers. "
+                      "Setting location to nan...")
+        latitude = np.nan
+        longitudue = np.nan
+
+    # depth
+    try:
+        depth = sac_headers.evdp
+    except (TypeError, ValueError):
+        warnings.warn("Could not determine event depth from sac headers. "
+                      "Setting depth to nan...")
+        depth = 0.
+
+    # origin time
+    try:
+        origin_time = obspy.UTCDateTime(
+            year=sac_headers.nzyear,
+            julday=sac_headers.nzjday, 
+            hour=sac_headers.nzhour, 
+            minute=sac_headers.nzmin,
+            second=sac_headers.nzsec) 
+    except (TypeError, ValueError):
+        warnings.warn("Could not determine origin time from sac headers. "
+                      "Setting origin time to zero...")
+        origin_time = obspy.UTCDateTime(0)
+
+    return (
+        latitude,
+        longitude,
+        depth,
+        origin_time.timestamp,
+        )
+
+    #return Origin(
+    #    time=origin_time,
+    #    longitude=sac_headers.evlo,
+    #    latitude=sac_headers.evla,
+    #    depth=depth * 1000.0,
+    #)
+
+
+def get_station_coords(sac_headers):
+    """ Creates station object based on sac metadata
+    """
+    # location
+    try:
+        latitude = sac_headers.stla
+        longitude = sac_headers.stlo
+    except:
+        warnings.warn("Could not determine station location from sac headers. "
+                      "Setting location to zero.")
+        latitude = 0.
+        longitude = 0.
+
+    # elevation
+    try:
+        elevation = sac_headers.stel
+        depth = 0.#depth = sac_headers.stdp
+    except:
+        warnings.warn("Could not determine station elevation from sac headers. "
+                      "Setting elevation to zero.")
+        elevation = 0.
+        depth = 0.
+    return (
+       latitude,
+       longitude,
+       elevation,
+       depth,
+       )
+
+
+def get_station_list(stream):
+    """ Creates station list from sac metadata
+    """
+    stations = []
+    for trace in stream:
+        station_name = trace.station
+        try:
+            latitude = trace.stats.sac.stla
+            longitude = trace.stats.sac.stlo
+            elevation = 0.
+        except:
+            warnings.warn("Could not determine station location from sac headers.")
+        stations += [Station(
+           station_name,
+           latitude,
+           longitude,
+           elevation)]
+    return stations
+
+
+
